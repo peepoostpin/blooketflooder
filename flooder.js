acb1cea0135deabd3af6098d535174f367365bef
berrybonk <91695764+peepoostpin@users.noreply.github.com>
2023-04-04 18:59:01 +0100

    Update flooder.js

--- flooder.js
+++ flooder.js
@@ -3,11 +3,11 @@ document.body.append(i);
 window.prompt = i.contentWindow.prompt.bind(window);
 i.remove();
 
-const id = prompt("Game ID - e.g = 8540099:");
 const name = prompt("Name - e.g = ItzMeFard:");
 const amount = parseInt(prompt("Amount - e.g = 50:"));
 const b = prompt("Blook - e.g = Mega Bot:");
 const bg = prompt("Banner -ALL LOWERCASE- e.g = chocolate:");
+const id = prompt("Game ID - e.g = 8540099:");
 
 console.log(b);
 console.log(bg);
